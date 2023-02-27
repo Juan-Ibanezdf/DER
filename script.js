b7035385aa8f380c4d5e45da6f85bf1410159417
Juan Ibanez <juan.ibanezdf@gmail.com>
2023-02-27 11:13:31 -0300

    Iniciando o projeto e criando o header

---
+++ script.js
@@ -0,0 +1,12 @@
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', ()=> {
+    document.body.classList.toggle('dark')
+})
+
+const chk1 = document.getElementById('chk1')
+
+chk1.addEventListener('change', ()=> {
+    document.body.classList.toggle('dark')
+})
+
